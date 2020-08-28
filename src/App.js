a4f9433cfcb49ea110da218f6891cb7ac61bad68
max bischof <max@bischof.dk>
2020-08-28 09:02:09 +0200

    Run prettier on all files

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { datasets } from './datasets.js'
+import React from "react"
+import logo from "./logo.svg"
+import "./App.css"
+import { datasets } from "./datasets.js"
 
 function App() {
   console.log(datasets)
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>
 }
 
-export default App;
+export default App
--- src/datasets.js
+++ src/datasets.js
@@ -1,12 +1,14 @@
 export const datasets = [
   {
-    name: 'Arbeitslosigkeit Kiel',
-    description: 'Arbeitslosigkeit in Kiel absolut und relativ seit 2009',
-    url: 'https://www.kiel.de/opendata/kiel_wirtschaft_arbeit_unterbeschaeftigung.csv',
-    imageUrl: 'https://images.unsplash.com/photo-1529400971008-f566de0e6dfc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80',
-    attributeKeys: '',
-    license: 'Datenlizenz Deutschland – Zero – Version 2.0',
-    publisher: 'Landeshauptstadt Kiel',
-    displayedAs: ''
-  }
+    name: "Arbeitslosigkeit Kiel",
+    description: "Arbeitslosigkeit in Kiel absolut und relativ seit 2009",
+    url:
+      "https://www.kiel.de/opendata/kiel_wirtschaft_arbeit_unterbeschaeftigung.csv",
+    imageUrl:
+      "https://images.unsplash.com/photo-1529400971008-f566de0e6dfc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80",
+    attributeKeys: "",
+    license: "Datenlizenz Deutschland – Zero – Version 2.0",
+    publisher: "Landeshauptstadt Kiel",
+    displayedAs: "",
+  },
 ]
