da45e47ee349bb648b1d6c32b8013958c09aac96
max bischof <max@bischof.dk>
2020-08-31 13:45:18 +0200

    Add header story

---
+++ src/stories/Header.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import HeaderComponent from '../components/Header'
+
+export default {
+  title: 'Header',
+  component: HeaderComponent,
+};
+
+export const Header = (args) => <HeaderComponent />;
