f624eb6a3767257b612072d020ae38bbee04c537
max bischof <max@bischof.dk>
2020-08-31 10:47:50 +0200

    Add Footer component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { datasets } from "./settings.js"
 import Header from "./components/Header"
 import BackButton from "./components/ui/BackButton.js"
 import DatasetDetails from "./components/DatasetDetails.js"
+import Footer from "./components/Footer.js"
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header/>
       <BackButton/>
       <DatasetDetails datasetDescription={datasets[0]} />
+      <Footer/>
     </>
   )
 }
