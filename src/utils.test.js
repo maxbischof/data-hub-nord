c91a768deefcc699b3bd9db8c81dbe8cc8f99211
max bischof <max@bischof.dk>
2020-09-01 09:34:23 +0200

    Update utils test with correct encoding

--- src/utils.test.js
+++ src/utils.test.js
@@ -1,24 +1,24 @@
 const { fetchCSV, csvToObjectsArray } = require("./utils")
 const { datasets } = require("./settings.js")
 
-const response = `Land;Stadt;Kategorie;Merkmal;Jahr;Unterbsch�ftigte;Unterbsch�ftigtenquote
-de-sh;Kiel;wirtschaft_arbeit;Unterbesch�ftigung;2009;18918;15,1
-de-sh;Kiel;wirtschaft_arbeit;Unterbesch�ftigung;2010;18414;14,4
-de-sh;Kiel;wirtschaft_arbeit;Unterbesch�ftigung;2011;18469;14,6
-de-sh;Kiel;wirtschaft_arbeit;Unterbesch�ftigung;2012;17565;13,5
-de-sh;Kiel;wirtschaft_arbeit;Unterbesch�ftigung;2013;17457;13,2
-de-sh;Kiel;wirtschaft_arbeit;Unterbesch�ftigung;2014;17468;13
-de-sh;Kiel;wirtschaft_arbeit;Unterbesch�ftigung;2015;17278;12,7
-de-sh;Kiel;wirtschaft_arbeit;Unterbesch�ftigung;2016;18633;13,5
-de-sh;Kiel;wirtschaft_arbeit;Unterbesch�ftigung;2017;17496;12,3
-de-sh;Kiel;wirtschaft_arbeit;Unterbesch�ftigung;2018;16608;11,5
-de-sh;Kiel;wirtschaft_arbeit;Unterbesch�ftigung;2019;15453;10,8
+const response = `Land;Stadt;Kategorie;Merkmal;Jahr;Unterbschäftigte;Unterbschäftigtenquote
+de-sh;Kiel;wirtschaft_arbeit;Unterbeschäftigung;2009;18918;15,1
+de-sh;Kiel;wirtschaft_arbeit;Unterbeschäftigung;2010;18414;14,4
+de-sh;Kiel;wirtschaft_arbeit;Unterbeschäftigung;2011;18469;14,6
+de-sh;Kiel;wirtschaft_arbeit;Unterbeschäftigung;2012;17565;13,5
+de-sh;Kiel;wirtschaft_arbeit;Unterbeschäftigung;2013;17457;13,2
+de-sh;Kiel;wirtschaft_arbeit;Unterbeschäftigung;2014;17468;13
+de-sh;Kiel;wirtschaft_arbeit;Unterbeschäftigung;2015;17278;12,7
+de-sh;Kiel;wirtschaft_arbeit;Unterbeschäftigung;2016;18633;13,5
+de-sh;Kiel;wirtschaft_arbeit;Unterbeschäftigung;2017;17496;12,3
+de-sh;Kiel;wirtschaft_arbeit;Unterbeschäftigung;2018;16608;11,5
+de-sh;Kiel;wirtschaft_arbeit;Unterbeschäftigung;2019;15453;10,8
 `
 
 test("fetch CSV from ODSH as string", () => {
   return fetchCSV({ path: datasets[0].url }).then((data) => {
     expect(data).toEqual(
-      expect.stringContaining(`Land;Stadt;Kategorie;Merkmal;Jahr;Unterbsch�ftigte;Unterbsch�ftigtenquote`)
+      expect.stringContaining(`Land;Stadt;Kategorie;Merkmal;Jahr;Unterbschäftigte;Unterbschäftigtenquote`)
     )
   })
 })
@@ -36,10 +36,10 @@ test("returns array of objects from csv text", () => {
         Jahr: "2009",
         Kategorie: "wirtschaft_arbeit",
         Land: "de-sh",
-        Merkmal: "Unterbesch�ftigung",
+        Merkmal: "Unterbeschäftigung",
         Stadt: "Kiel",
-        "Unterbsch�ftigte": "18918",
-        "Unterbsch�ftigtenquote": "15,1"
+        "Unterbschäftigte": "18918",
+        "Unterbschäftigtenquote": "15,1"
       },
     ])
   )
