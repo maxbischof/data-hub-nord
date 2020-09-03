24b31889948f565ed254d1ede1e3d08a4699dbc1
max bischof <max@bischof.dk>
2020-09-03 11:08:45 +0200

    Add WelcomeSection story

---
+++ src/stories/WelcomeSection.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import WelcomeSectionComponent from '../components/WelcomeSection'
+
+export default {
+  title: 'WelcomeSection',
+  component: WelcomeSectionComponent,
+};
+
+export const WelcomeSection = () => <WelcomeSectionComponent />
