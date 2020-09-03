b21ee19f5c8f416d4445b54d3422fc66aaf14d67
max bischof <max@bischof.dk>
2020-09-03 10:51:36 +0200

    Add DatasetList component

---
+++ src/components/DatasetList.js
@@ -0,0 +1,40 @@
+import React, { useRef } from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import DatasetTeaser from './DatasetTeaser'
+
+export default function DataList({ datasets, setRef }) {
+  const headlineRef = useRef(null)
+  setRef(headlineRef)
+
+  return (
+    <StyledDatasetList>
+      <h2 ref={headlineRef}>Datensätze</h2>
+      {datasets.map((dataset) => (
+        <Link
+          to={`/datensaetze/${dataset.name.replace(" ", "-")}-${dataset.id}`}
+          key={dataset.id}
+        >
+          <DatasetTeaser
+            imagePath={dataset.imageUrl}
+            title={dataset.name}
+            description={dataset.description}
+          />
+        </Link>
+      ))}
+    </StyledDatasetList>
+  )
+}
+
+const StyledDatasetList = styled.section`
+  flex: 1 0 auto;
+  padding: 50px 37px 30px 37px;
+
+  h2 {
+    font-size: 25px;
+    padding: 0 0 5px 0;
+    border-bottom: 1px solid var(--grey);
+    display: inline-block;
+    margin: 0 0 30px 0;
+  }
+`
--- src/components/pages/RootPage.js
+++ src/components/pages/RootPage.js
@@ -1,47 +1,19 @@
-import React, { useRef } from 'react'
-import DatasetTeaser from "../DatasetTeaser"
-import { Link } from "react-router-dom"
-import styled from "styled-components"
+import React, { useRef, useState } from 'react'
 import WelcomeSection from '../WelcomeSection'
+import DatasetList from '../DatasetList'
 
-export default function RootPage({datasets}) {
-  const datasetSectionRef = useRef(null)
+export default function RootPage({ datasets }) {
+  const [datasetListRef, setDatasetListRef] = useState()
+
   const scrollToRef = (ref) => {
     console.log("scroll")
     window.scrollTo(0, ref.current.offsetTop)
   }
-  
+
   return (
     <>
-      <WelcomeSection onClickButton={scrollToRef} scrollTo={datasetSectionRef}/>
-      <DatasetList>
-        <h2 ref={datasetSectionRef}>Datensätze</h2>
-        {datasets.map((dataset) => (
-          <Link
-            to={`/datensaetze/${dataset.name.replace(" ", "-")}-${dataset.id}`}
-            key={dataset.id}
-          >
-            <DatasetTeaser
-              imagePath={dataset.imageUrl}
-              title={dataset.name}
-              description={dataset.description}
-            />
-          </Link>
-        ))}
-      </DatasetList>
+      <WelcomeSection onClickButton={scrollToRef} scrollTo={datasetListRef}/>
+      <DatasetList datasets={datasets} setRef={setDatasetListRef}/>
     </>
   )
 }
-
-const DatasetList = styled.section`
-  flex: 1 0 auto;
-  padding: 50px 37px 30px 37px;
-
-  h2 {
-    font-size: 25px;
-    padding: 0 0 5px 0;
-    border-bottom: 1px solid var(--grey);
-    display: inline-block;
-    margin: 0 0 30px 0;
-  }
-`
