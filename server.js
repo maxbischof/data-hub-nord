28cecc358a484ac605f9bc340f30a5a37bee6458
max bischof <max@bischof.dk>
2020-09-13 11:36:14 +0200

    Restructure dataset objects

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-var corsAnywhere = require('cors-anywhere')
+const corsAnywhere = require('cors-anywhere')
 const fetch = require('node-fetch')
-var parseString = require('xml2js').parseString
+const parseString = require('xml2js').parseString
 
 const port = process.env.PORT || 3001
 
@@ -13,7 +13,7 @@ app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`)
 })
 
-let proxy = corsAnywhere.createServer({
+const proxy = corsAnywhere.createServer({
   originWhitelist: [], // Allow all origins
   requireHeaders: [], // Do not require any headers.
   removeHeaders: [], // Do not remove any headers.
@@ -38,34 +38,59 @@ fetch('https://opendata.schleswig-holstein.de/catalog.xml')
   .then((response) => response.text())
   .then((data) => {
     parseString(data, (err, result) => {
-      const pages = result['rdf:RDF']['hydra:PagedCollection'][0][
+      const pagesTotal = result['rdf:RDF']['hydra:PagedCollection'][0][
         'hydra:lastPage'
       ][0].split('=')[1]
-      fetchDatasets(pages).then((response) => (datasets = response))
-      //console.log(datasets)
+      fetchDatasets(pagesTotal).then(
+        (response) => (datasets = restructureDatasetObjects(response))
+      )
     }).catch(function (error) {
-      console.log('Fehler: bei Auslesen der XML-Datei ' + error)
+      console.log(error)
     })
   })
 
-async function fetchDatasets(pages) {
+function restructureDatasetObjects(datasets) {
+  return datasets.map((dataset) => {
+    innerDataset = dataset['dcat:Dataset'][0]
+    dataset.url = innerDataset['dcat:distribution'][0]['$']['rdf:resource']
+    dataset.title = innerDataset['dct:title'][0]
+    dataset.publisher = innerDataset['dcatde:licenseAttributionByText']
+      ? innerDataset['dcatde:licenseAttributionByText'][0]
+      : ''
+    dataset.publisherURL = innerDataset['dct:publisher'][0]['$']['rdf:resource']
+    dataset.description = innerDataset['dct:description'][0]
+    dataset.license = innerDataset['dct:license'][0]['$']['rdf:resource']
+    delete dataset['dcat:Dataset']
+    return dataset
+  })
+}
+
+async function fetchDatasets(pagesTotal) {
   let response = []
-  for (var i = 1; i <= pages; i++) {
-    console.log(i)
+
+  for (let i = 1; i <= pagesTotal; i++) {
+    console.log('Loading XML page: ' + i)
+
     await fetch('https://opendata.schleswig-holstein.de/catalog.xml?page=' + i)
       .then((response) => response.text())
       .then((text) => {
         parseString(text, (err, XMLObjects) => {
           const datasets =
             XMLObjects['rdf:RDF']['dcat:Catalog'][0]['dcat:dataset']
-          const filteredDatasets = datasets.filter((dataset) =>
-            dataset['dcat:Dataset'][0]['dcat:distribution'][0]['$'][
-              'rdf:resource'
-            ].endsWith('csv')
-          )
+          const filteredDatasets = filterByFileType('csv', datasets)
+
           response = [...response, ...filteredDatasets]
         })
       })
   }
+
   return response
+}
+
+function filterByFileType(fileType, data) {
+  return data.filter((dataset) =>
+    dataset['dcat:Dataset'][0]['dcat:distribution'][0]['$'][
+      'rdf:resource'
+    ].endsWith(fileType)
+  )
 }
