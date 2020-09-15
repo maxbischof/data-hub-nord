dd68e4e229f4756ec8eb09de8b602149af923dce
max bischof <max@bischof.dk>
2020-09-15 17:11:52 +0200

    Style searchform

--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import styled from 'styled-components'
+import { ReactComponent as SearchIcon } from '../icons/search.svg'
 
 export default function SearchForm({ searchTerm, setSearchTerm }) {
   let timeout = null
@@ -13,11 +15,37 @@ export default function SearchForm({ searchTerm, setSearchTerm }) {
   }
 
   return (
-    <form>
-      <label>
-        Suche
-        <input type="text" onChange={handleChange} />{' '}
-      </label>
-    </form>
+    <Form>
+      <Label>
+        <SearchIcon />
+        <Input type="text" onChange={handleChange} />{' '}
+      </Label>
+    </Form>
   )
 }
+
+const Form = styled.form`
+  border-radius: 20px;
+  margin: 20px;
+  align-self: center;
+  padding: 5px;
+  border: 1px solid var(--grey);
+  width: 300px;
+`
+
+const Label = styled.label`
+  display: flex;
+  align-items: center;
+`
+
+const Input = styled.input`
+  width: 100%;
+  margin-left: 5px;
+  border-top-style: hidden;
+  border-right-style: hidden;
+  border-left-style: hidden;
+  border-bottom-style: hidden;
+  &:focus {
+    outline: none;
+  }
+`
