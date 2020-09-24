6ad954910652375c9e511eb4178782b368f1d095
max bischof <max@bischof.dk>
2020-09-24 09:42:11 +0200

    Move corsanywhere server to seperate file

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const corsAnywhere = require('cors-anywhere')
 const fetch = require('node-fetch')
 const parseString = require('xml2js').parseString
+require('./corsAnywhere')(app)
 
 const port = process.env.PORT || 3001
 
@@ -11,17 +11,6 @@ app.use(express.static(path.join(__dirname, 'client/build')))
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`)
-})
-
-const proxy = corsAnywhere.createServer({
-  originWhitelist: [], // Allow all origins
-  requireHeaders: [], // Do not require any headers.
-  removeHeaders: [], // Do not remove any headers.
-})
-
-app.get('/proxy/:proxyUrl*', (req, res) => {
-  req.url = req.url.replace('/proxy/', '/')
-  proxy.emit('request', req, res)
 })
 
 app.get('/datasets', (req, res) => {
